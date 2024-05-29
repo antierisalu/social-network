0043fd3beb71a00a06adc668c19ba1ad0c478887
lhaavel <lukas.haavel@gmail.com>
2024-05-29 12:44:29 +0300

    server setup

---
+++ backend/pkg/auth.go
@@ -0,0 +1,12 @@
+package pkg
+
+import (
+	"fmt"
+	"net/http"
+)
+
+//KOIK LOGIN/REGISTER/AUTH HANDLERID SIIA
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("TSAU TERE LOGINIJA")
+}
--- backend/server.go
+++ backend/server.go
@@ -1,15 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"backend/pkg"
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("DASDASD")
-	fmt.Println("DASDASD")
-	fmt.Println("DASDASD")
-	fmt.Println("DASDASD")
-	fmt.Println("DASDASD")
-	fmt.Println("DASDASD")
-	fmt.Println("DASDASD")
-	fmt.Println("DASDASD")
-	fmt.Println("DASDASD")
+	// Serve static files from the current directory
+	http.Handle("/", http.FileServer(http.Dir("../public")))
+
+	http.HandleFunc("/login", pkg.LoginHandler)
+
+	// Start the server on port 8080
+	fmt.Println("SN is running on http://localhost:8080/")
+	http.ListenAndServe(":8080", nil)
 }
