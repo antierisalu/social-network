27c9dab4dcb12e73b2b793b0e1a0be3ddbd70c72
lhaavel <lukas.haavel@gmail.com>
2024-05-29 12:14:29 +0300

    more setup

---
+++ backend/server.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("DASDASD")
+	fmt.Println("DASDASD")
+	fmt.Println("DASDASD")
+	fmt.Println("DASDASD")
+	fmt.Println("DASDASD")
+	fmt.Println("DASDASD")
+	fmt.Println("DASDASD")
+	fmt.Println("DASDASD")
+	fmt.Println("DASDASD")
+}
