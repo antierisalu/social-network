821e2c2c9e7f0029adfb26e11151db26082b65bb
antierisalu <antikaz@gmail.com>
2024-06-03 16:08:14 +0300

    profile buttons and if

--- src/components/profile.svelte
+++ src/components/profile.svelte
@@ -14,35 +14,44 @@ import Button from "../shared/button.svelte";
             <!-- {/if} -->
             <Button type="secondary" w84={true} id="chatBtn">Chat</Button>
         </div>
-        
-        <div class="birthday">Birthday</div>
-        <div class="aboutme">About me</div>
-        <div class="posts">User activity
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
+        <div class="birthdayContainer">
+            <label for="birthday">Birthday: </label>
+            <div class="birthday"> 04-05-2024</div>
+        </div>
+        <label for="aboutme">About me:</label>
+        <div class="aboutme">Every once in a while something amazing comes around, so here i am :)))</div>
+        <div class="posts">
+            <li>Accepted follower request</li>
+            <li>Posted to "Märgatud Viljandis"</li>
+            <li>Commented Albert Einstein post</li>
+            <li>Created Group "Hack the Planet"</li>
+            <li>Invited you to the group "DIY"</li>
+            <li>Invited you to the group "Big fat dark marketplace for car keys"</li>
+            <li>Created Event "Jaanid 2024"</li>
         </div>
     </div>
 </main>
 
 <style>
+    div {
+        padding-bottom: 8px;
+    }
 
     main {
-        padding: 8px;
-        color: greenyellow;
+        padding: 4px;
+        color: rgba(172, 255, 47, 0.616);
         display: flex;
         flex-direction: column;
         text-align: center;
     }
 
-    
-
     .avatar {
-        height: 300px;
+        height: 278px;
+    }
 
-
+    .birthdayContainer {
+        display: flex;
+        justify-content: space-around;
 
     }
 
