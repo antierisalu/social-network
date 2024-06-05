b6621556adbe104d07a968a449b246ffa2564b24
Anti Erisalu <antikaz@gmail.com>
2024-06-05 16:27:49 +0300

    profile with fetching own data, avatar hard coded, activity not working

--- src/components/profile.svelte
+++ src/components/profile.svelte
@@ -1,14 +1,39 @@
 <script>
-import Button from "../shared/button.svelte";
-</script>
+    import { onMount } from 'svelte';
+    import Button from "../shared/button.svelte";
+
+    let notOwnPage
+
+    let user = {
+        id: '',
+        email: '',
+        firstName: '',
+        lastName: '',
+        dateOfBirth: '',
+        avatar: '',
+        nickName: '',
+        aboutMe: '',
+    };
+
+    onMount(async () => {
+        try {
+            const response = await fetch('http://localhost:8080/session'); // Replace with your actual endpoint
+            const data = await response.json();
+            user = data;
+            user.avatar = "./avatars/av.png"
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+        }
+    });</script>
 
 <main>
+    
     <div class="userContainer">
-        <div class="name">FirstName LastName</div>
+        <div class="name">{user.firstName} {user.lastName} {#if user.nickName}({user.nickName}){/if}</div>
         <div class="avatar">
-            <img src="../avatars/av.png" border="0" alt="av.png" />
+            <img src={user.avatar} border="0" alt="avatar" />
         </div>
-        <!-- {#if notOwnPage} -->
+        {#if notOwnPage}
         <div class="buttons">
             <!-- {#if followingUser }  -->
                 <Button id="unFollowBtn">unFollow</Button>
@@ -17,61 +42,30 @@ import Button from "../shared/button.svelte";
             <!-- {/if} -->
             <Button type="secondary" inverse={true} w84={true} id="chatBtn">Chat</Button>
         </div>
-        <!-- {/if} -->
-            <div class="birthday">Birthday: 04-05-2000</div>
+        {/if}
+            <div class="birthday">Birthday: {user.dateOfBirth}</div>
+            {#if user.aboutMe}
         <label for="aboutMe">About me:</label>
-        <div class="aboutMe">Every once in a while something amazing comes around, so here i am :)))Every once in a while something amazing comes around, so here i am :)))</div>
+        <div class="aboutMe">{user.aboutMe}</div>
+            {/if}
+        <label for activity>Latest posts</label>
         <div class="activity">
-            <div>Accepted follower request</div>
             <div>Posted to "Märgatud Viljandis"</div>
-            <div>Commented Albert Einstein post</div>
-            <div>Created Group "Hack the Planet"</div>
-            <div>Invited you to the group "DIY"</div>
-            <div>Invited you to the group "Big fat dark marketplace for car keys"</div>
-            <div>Created Event "Jaanid 2024"</div>
-            <div>Accepted follower request</div>
             <div>Posted to "Märgatud Viljandis"</div>
-            <div>Commented Albert Einstein post</div>
-            <div>Created Group "Hack the Planet"</div>
-            <div>Invited you to the group "DIY"</div>
-            <div>Invited you to the group "Big fat dark marketplace for car keys"</div>
-            <div>Created Event "Jaanid 2024"</div>
-            <div>Accepted follower request</div>
             <div>Posted to "Märgatud Viljandis"</div>
-            <div>Commented Albert Einstein post</div>
-            <div>Created Group "Hack the Planet"</div>
-            <div>Invited you to the group "DIY"</div>
-            <div>Invited you to the group "Big fat dark marketplace for car keys"</div>
-            <div>Created Event "Jaanid 2024"</div>
-            <div>Accepted follower request</div>
-            <div>Posted to "Märgatud Viljandis"</div>
-            <div>Commented Albert Einstein post</div>
-            <div>Created Group "Hack the Planet"</div>
-            <div>Invited you to the group "DIY"</div>
-            <div>Commented Albert Einstein post</div>
-            <div>Created Group "Hack the Planet"</div>
-            <div>Invited you to the group "DIY"</div>
-            <div>Commented Albert Einstein post</div>
-            <div>Created Group "Hack the Planet"</div>
-            <div>Invited you to the group "DIY"</div>
-            <div>Commented Albert Einstein post</div>
-            <div>Created Group "Hack the Planet"</div>
-            <div>Invited you to the group "DIY"</div>
-            <div>Invited you to the group "Big fat dark marketplace for car keys"</div>
-            <div>Created Event "Jaanid 2024"</div>
         </div>
     </div>
 </main>
 
 <style>
 
-    main {
-        padding: 4px;
+
+main {
+        padding: 8px;
         color: rgba(172, 255, 47, 0.616);
         display: flex;
         flex-direction: column;
-        text-align: center;
-        max-height: 90vh;
+        font-size: small;
     }
 
     div {
@@ -81,23 +75,22 @@ import Button from "../shared/button.svelte";
     img {
         max-width: 280px;
     }
+    label {
+        padding: 8px;
+    }
 
-    .aboutMe {
+    .aboutMe, .activity{
+        font-size: small;
         max-height: 200px;
         overflow: auto;
+        border: solid 1px greenyellow;
+        border-radius: 6px;
+        text-align: left;
+        padding: 8px;
     }
 
     .activity {
-        overflow: auto;
-        /* max-height: 600px;  */
-        height: 55vh; /*i dont know about that*/
-        padding: 4px;
-        display: flex;
-        flex-direction: column;
-        border-radius: 6px;
-        border-color: greenyellow;
-        border: solid 1px greenyellow;
-        text-align: left;
+        max-height: 500px;
     }
     
 
