c1a27feeef643466b57fc13b6efae047231842fb
antierisalu <antikaz@gmail.com>
2024-06-19 17:13:42 +0300

    too large boxes

--- src/components/posts/posts.svelte
+++ src/components/posts/posts.svelte
@@ -17,6 +17,50 @@
   function toggleComments() {
     showComments = !showComments;
   }
+
+  const posts = [
+    {
+        content: "Hello, world! This is my first post.",
+        createdBy: "Alice",
+        createdAt: "2024-06-19 16:30:00",
+        comments: [
+            { createdBy: "Eve", createdAt: "2024-06-19 16:35:00", content: "Nice start!" },
+            { createdBy: "Bob", createdAt: "2024-06-19 16:40:00", content: "Keep it up!" },
+            { createdBy: "Carol", createdAt: "2024-06-19 16:45:00", content: "Looking forward to more." }
+        ]
+    },
+    {
+        content: "Exciting news! 🎉",
+        createdBy: "Bob",
+        createdAt: "2024-06-19 17:15:00",
+        comments: [
+            { createdBy: "Alice", createdAt: "2024-06-19 17:20:00", content: "What's the news?" },
+            { createdBy: "Eve", createdAt: "2024-06-19 17:25:00", content: "Share the details!" },
+            { createdBy: "Dan", createdAt: "2024-06-19 17:30:00", content: "Congratulations!" }
+        ]
+    },
+    {
+        content: "Exploring the Hidden Gems of Kyoto 🌸",
+        createdBy: "Emily",
+        createdAt: "2024-06-19 19:30:00",
+        comments: [
+            { createdBy: "Nora", createdAt: "2024-06-19 19:35:00", content: "Kyoto is on my bucket list!" },
+            { createdBy: "Henry", createdAt: "2024-06-19 19:40:00", content: "Share your favorite spots!" },
+            { createdBy: "Isabella", createdAt: "2024-06-19 19:45:00", content: "Beautiful photos!" }
+        ]
+    },
+    {
+        content: "Delicious Chocolate Chip Cookies Recipe 🍪",
+        createdBy: "Grace",
+        createdAt: "2024-06-19 18:00:00",
+        comments: [
+            { createdBy: "Liam", createdAt: "2024-06-19 18:05:00", content: "I love chocolate chip cookies!" },
+            { createdBy: "Sophie", createdAt: "2024-06-19 18:10:00", content: "Can't wait to try this recipe!" },
+            { createdBy: "Oliver", createdAt: "2024-06-19 18:15:00", content: "Thanks for sharing!" }
+        ]
+    },
+
+  ]
 </script>
 
 <main>
@@ -27,21 +71,19 @@
   <!-- svelte-ignore a11y-click-events-have-key-events -->
   <div class="createPost" on:click={toggleOverlay}>Create new Post..</div>
   <!-- svelte-ignore a11y-click-events-have-key-events -->
-  <div class="singlePost" >
+   {#each posts as post}
+    <div class="singlePost" >
       <div class="userInfo" on:click={openProfile()}>
-        <img src={$userInfo.avatar} alt="user avatar" />
-        <p class="username">{$userInfo.firstName} {$userInfo.lastName}</p>
-        <p class="createdAt">post.CreatedAt</p>
+        <p class="postCreatorAvatar"> <img src={$userInfo.avatar} alt="user avatar" /></p>
+        <p class="postCreator">{post.createdBy}</p>
+        <p class="postCreatedAt">{post.createdAt}</p>
       </div>
+      <div class="postContent" on:click={toggleComments}>{post.content}</div>
 
-    <div class="postContent" on:click={toggleComments}>
-      This is post, i like turtles and please jõuludeks mulle "kolm kotti täis
-      viiesajaseid" Click for comments
-    </div>
     {#if showComments}
       <div in:slide class="addComment">
         <textarea placeholder="Comment post.."></textarea>
-        <div class="postButtons">
+        <div class="commentButtons">
           <Button type="secondary">Comment</Button>
           <ImageToComment
             inputIDProp="commentImage"
@@ -50,77 +92,35 @@
         </div>
       </div>
       <div class="comments">
-        <div class="singleComment">
-          <p class="username">{$userInfo.firstName} {$userInfo.lastName}</p>
-          <p class="createdAt">comment.CreatedAt</p>
-          <div class="commentContent">Ma ei usu, et sulle seda võimalik anda on. comment.Content</div>
-        </div>
-        <div class="singleComment">
-          <p class="username">{$userInfo.firstName} {$userInfo.lastName}</p>
-          <p class="createdAt">comment.CreatedAt</p>
-          <div class="commentContent">Ma ei usu, et sulle seda võimalik anda on. comment.Content</div>
-        </div>  <div class="singleComment">
-          <p class="username">{$userInfo.firstName} {$userInfo.lastName}</p>
-          <p class="createdAt">comment.CreatedAt</p>
-          <div class="commentContent">Ma ei usu, et sulle seda võimalik anda on. comment.Content</div>
-        </div>
+        {#each post.comments as comment}
+          <div class="singleComment">
+            <div class="userInfo" on:click={openProfile()}>
+              <p class="commentCreator">{comment.createdBy}</p>
+              <p class="commentCreatorAvatar"><img src={$userInfo.avatar} alt="user avatar" /></p>
+              <p class="commentCreatedAt">{comment.createdAt}</p>
+            </div>
+            <div class="commentContent">{comment.content}</div>
+          </div>
+          {/each}
       </div>
     {/if}
   </div>
+  {/each}
 </main>
 
 <style>
   main {
+    display: flex;
+    flex-direction: column;
     padding: 4px;
     border-radius: 8px;
     border: solid 1px #333;
   }
 
-  div {
+ div {
     border: solid 1px #333;
     padding: 8px;
     border-radius: 8px;
-  }
-
-  .singlePost {
-    display: grid;
-  }
-  
-  /* post creator stuff */
-
-  .userInfo {
-    display: grid;
-    align-items: center;
-    grid-column: 1;
-  }
-
-  .username {
-    grid-row: 1;
-    grid-column: 1;
-  }
-
-  img {
-    grid-row: 2;
-    grid-column: 1;
-    border-radius: 50px;
-    max-width: 90px;
-  }
-  .createdAt {
-    grid-row: 3;
-    grid-column: 1;
-  }
-
-  /* post creator stuff end  */
-
-  .postContent {
-    grid-column: 2;
-    display: grid;
-  }
-
-  .postButtons {
-    padding: 0;
-    margin-top: 0.5em;
-    border: none;
   }
 
   .createPost {
@@ -131,34 +131,48 @@
     padding: 8px;
     margin: 4px;
   }
-
   .createPost:hover {
     cursor: pointer;
   }
 
-  .addComment {
-    grid-row: 2;
-    /* grid-row: 2; */
+  img {
+    max-height: 90px; width: 90px
   }
 
-  .comments {
-    grid-row:3
+  textarea {
+    width: 100%;
+    min-height: 100px;
   }
 
-  /* .addComment textarea {
-    margin-right: 4px;
-    flex-grow: 1;
-    border-radius: 8px;
-    min-height: 80px;
-    margin-bottom: 0;
-    padding: 4px;
-    text-align: left;
-    vertical-align: top;
-    resize: none;
-    overflow: auto;
-  } */
+  .singlePost {
+  display: grid; 
+  grid-auto-columns: 1fr; 
+  grid-template-columns: 0.3fr 1.5fr; 
+  grid-template-rows: 0.5fr 0.5fr 3fr; 
+  gap: 0px 0px; 
+  grid-template-areas: 
+    "userInfo postContent"
+    "addComment addComment"
+    "comments comments"; 
+}
+.postContent { grid-area: postContent; }
+.userInfo { grid-area: userInfo; }
+.addComment { grid-area: addComment; }
+.comments { grid-area: comments; }
 
-  .comment {
-    margin: 2px;
-  }
+.singleComment {
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-template-columns: 0.3fr 1.5fr; 
+  grid-template-rows: 0.5fr 0.5fr 3fr; 
+  grid-template-areas: 
+    "userInfo commentContent"
+    "userInfo commentContent"
+    "userInfo commentContent"; 
+}
+
+.commentContent { grid-area: commentContent }
+
+
+
 </style>
